8a23ebd0d77ae04a41fcbf0faa6557dc13cde00a
astrolion <aslitroon@gmail.com>
2020-02-07 12:34:42 +0600

    still some logical error

--- dp/1600-1800/1278c.cpp
+++ dp/1600-1800/1278c.cpp
@@ -10,68 +10,47 @@ int main() {
         int n;
 
         scanf("%d", &n);
-        vector<int>L(n), R(n);
+        vector< int > v(n + n);
 
-        for(auto &it : L) scanf("%d", &it);
-        for(auto &it : R) scanf("%d", &it);
+        for(auto &it : v) scanf("%d", &it);
 
-        int total_one = 0, total_two = 0;
-        for(auto it : L) total_one += (it == 1), total_two += (it == 2);
-        for(auto it : R) total_one += (it == 1), total_two += (it == 2);
+        int cur = n + n;
+        for(auto it : v) cur +=  it == 1  ? +1 : -1;
 
-        if(total_one == total_two) {
-            puts("0");
-            continue;
+        int base = n + n;
+
+        map<int, int > mp;
+        int test = n + n;
+        mp[test] = 0;
+
+        for(int i = n; i < n + n; i++) {
+            test += v[i] == 1 ? +1 : -1;
+            if( mp.find(test)  == mp.end() ) {
+                mp[test] = i - n + 1;
+            }
         }
 
-        if(total_one == 0 or total_two == 0) {
-            printf("%d\n", n + n);
-            continue;
-        }
 
 
+        test = n + n;
         int ans = n + n;
-
-        int one = 0, two = 0;
-        for(int i = 1; i <= n; i++) {
-            if( L[n - i] == 1 ) one++;
-            else two++;
-            if( total_one - one == total_two - two ) {
-                ans = min(ans, i);
-            }
+        if( mp.find(cur - test + base) != mp.end() ) {
+            ans = min(ans, mp[cur - test] );
         }
 
-        one = two = 0;
-        for(int i = 0; i < n; i++) {
-            if( R[i] == 1 ) one++;
-            else two++;
-
-            if( total_one - one == total_two - two ) {
-                ans = min(ans, i + 1);
-            }
-
-        }
-
-        vector< int > dp(n + n + 5, -1);
-
-        int cur = n;
-
+        cout << endl;
+        int cnt = 0;
         for(int i = n - 1; i >= 0; i--) {
-            if(R[i] == 1) cur++;
-            else cur--;
-            dp[cur] = i;
-        }
-
-        cur = n;
-        for(int i = 0; i < n; i++) {
-            if(L[i] == 2) cur++;
-            else cur--;
-            if( dp[cur] ^ -1 ) {
-                ans = min(ans, n - i - 1 + dp[cur] );
+            test += v[i] == 1 ? +1 : -1;
+            cnt++;
+            cout << test << endl;
+            if( mp.find(cur - test+base) != mp.end() ) {
+                ans = min(ans, mp[cur - test] + cnt );
             }
         }
 
         printf("%d\n", ans);
+
 
     }
 
